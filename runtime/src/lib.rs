07e1a7209f706c01d68c41ba6fc364910c286d3c
4meta5 <asinghchrony@protonmail.com>
2024-04-18 19:16:52 -0400

    import directly from frontier

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -84,7 +84,7 @@ use crate::{
 
 /// Alias to 512-bit hash when used in the context of a transaction signature on
 /// the chain.
-pub type Signature = MultiSignature;
+pub type Signature = fp_ethereum::EthereumSignature;
 
 /// Some way of identifying an account on the chain. We intentionally make it
 /// equivalent to the public key of our transaction signing scheme.
