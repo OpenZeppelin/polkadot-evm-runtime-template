46018207338f4850b58341044baa8de6a8dfd30f
4meta5 <asinghchrony@protonmail.com>
2024-04-23 16:56:35 -0400

    save

--- runtime/src/xcm_config.rs
+++ runtime/src/xcm_config.rs
@@ -137,6 +137,34 @@ pub type Barrier = TrailingSetTopicAsId<
     >,
 >;
 
+use frame_support::traits::{Currency, OnUnbalanced};
+
+pub struct ToAuthor<R>(sp_std::marker::PhantomData<R>);
+// use frame_support::traits::fungible::{Balanced, Credit},
+// impl<R> OnUnbalanced<Credit<R::AccountId, pallet_balances::Pallet<R>>> for ToAuthor<R>
+// where
+//     R: pallet_balances::Config + pallet_authorship::Config,
+//     pallet_balances::Pallet<R> as Currency<R::AccountId>>::NegativeImbalance: From<Credit<R::AccountId, pallet_balances::Pallet<R>>>;
+// {
+//     fn on_nonzero_unbalanced(
+//         amount: Credit<<R as frame_system::Config>::AccountId, pallet_balances::Pallet<R>>,
+//     ) {
+//         if let Some(author) = <pallet_authorship::Pallet<R>>::author() {
+//             let _ = <pallet_balances::Pallet<R>>::resolve(&author, amount);
+//         }
+//     }
+// }
+impl<R> OnUnbalanced<<pallet_balances::Pallet<R> as Currency<R::AccountId>>::NegativeImbalance>
+    for ToAuthor<R>
+where
+    R: pallet_balances::Config + pallet_authorship::Config,
+{
+    fn on_nonzero_unbalanced(
+        _amount: <pallet_balances::Pallet<R> as Currency<R::AccountId>>::NegativeImbalance,
+    ) { /*drop negative imbalances*/
+    }
+}
+
 pub struct XcmConfig;
 impl xcm_executor::Config for XcmConfig {
     type Aliasers = Nothing;
@@ -159,7 +187,8 @@ impl xcm_executor::Config for XcmConfig {
     type RuntimeCall = RuntimeCall;
     type SafeCallFilter = Everything;
     type SubscriptionService = PolkadotXcm;
-    type Trader = UsingComponents<WeightToFee, RelayLocation, AccountId, Balances, ()>;
+    type Trader =
+        UsingComponents<WeightToFee, RelayLocation, AccountId, Balances, ToAuthor<Runtime>>;
     type TransactionalProcessor = FrameTransactionalProcessor;
     type UniversalAliases = Nothing;
     // Teleporting is disabled.
