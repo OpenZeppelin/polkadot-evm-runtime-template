dcbc557d1f55300a21f3f61d7d85fdbc9b300111
4meta5 <asinghchrony@protonmail.com>
2024-04-23 11:05:02 -0400

    get runtime compiling

--- runtime/src/xcm_config.rs
+++ runtime/src/xcm_config.rs
@@ -137,27 +137,6 @@ pub type Barrier = TrailingSetTopicAsId<
     >,
 >;
 
-use frame_support::traits::{
-    fungible::{Balanced, Credit},
-    OnUnbalanced,
-};
-
-/// Logic for the author to get a portion of fees.
-/// TODO: move to XCM Config above where it is used
-pub struct ToAuthor<R>(sp_std::marker::PhantomData<R>);
-impl<R> OnUnbalanced<Credit<R::AccountId, pallet_balances::Pallet<R>>> for ToAuthor<R>
-where
-    R: pallet_balances::Config + pallet_authorship::Config,
-{
-    fn on_nonzero_unbalanced(
-        amount: Credit<<R as frame_system::Config>::AccountId, pallet_balances::Pallet<R>>,
-    ) {
-        if let Some(author) = <pallet_authorship::Pallet<R>>::author() {
-            let _ = <pallet_balances::Pallet<R>>::resolve(&author, amount);
-        }
-    }
-}
-
 pub struct XcmConfig;
 impl xcm_executor::Config for XcmConfig {
     type Aliasers = Nothing;
@@ -180,8 +159,7 @@ impl xcm_executor::Config for XcmConfig {
     type RuntimeCall = RuntimeCall;
     type SafeCallFilter = Everything;
     type SubscriptionService = PolkadotXcm;
-    type Trader =
-        UsingComponents<WeightToFee, RelayLocation, AccountId, Balances, ToAuthor<Runtime>>;
+    type Trader = UsingComponents<WeightToFee, RelayLocation, AccountId, Balances, ()>;
     type TransactionalProcessor = FrameTransactionalProcessor;
     type UniversalAliases = Nothing;
     // Teleporting is disabled.
